3b7fc27114cab335cd758b92bb6586642ca627e3
Teo Nogueira <teonogueira17@gmail.com>
2022-05-07 14:06:53 -0300

    index.php docked for xampp

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>PHP INITIALIZATION</title>
+</head>
+
+<body>
+    <?php 
+
+for ($i = 0; $i < 10 ; $i++) {
+
+
+print("Line" . $i . "<br />")
+
+}
+
+?>
+</body>
+
+
+</html>

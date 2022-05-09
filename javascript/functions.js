7d67901f999960a5040a880bd39d75dada3a61e2
Teo Nogueira <teonogueira17@gmail.com>
2022-05-09 14:58:17 -0300

    node

---
+++ javascript/functions.js
@@ -0,0 +1,35 @@
+function execute(a, b) {
+
+return a + b
+
+}
+
+const recipe =  execute(500, 20)
+
+// console.log('My Gold: ' + recipe)
+
+// FN + Loops
+
+// Criando entrada para arrays e filtrando apenas números pares.
+
+function activeValue(arr) {
+
+    let evenNums = []
+
+for(let i = 0; i < arr.length; i++) {
+
+    if(arr[i] % 2 == 0) {
+
+        evenNums.push(arr[i])
+
+    }
+
+}
+console.log(evenNums)
+
+}
+
+const socket = [50, 23, 25, 26, 41, 60]
+
+
+activeValue(socket)

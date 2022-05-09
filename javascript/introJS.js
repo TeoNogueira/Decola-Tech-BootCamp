7eae38b1d767f881f3721a208d8e41c707e56b8f
Teo Nogueira <teonogueira17@gmail.com>
2022-05-09 14:41:29 -0300

    introduction_js

---
+++ javascript/introJS.js
@@ -0,0 +1,6 @@
+//Comentário única linha
+/* Comentário linhas infinitas até o fechamento do comentário,
+ usa-se muito dentro do código css, js*/
+ // <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --> comentário para linguagem de marcação html 
+
+ 

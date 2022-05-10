d8410b692e222e4b66a55622e15e72f5a9316681
Teo Nogueira <teonogueira17@gmail.com>
2022-05-09 21:38:04 -0300

    calc

---
+++ Sintaxe Básica Javascript/5 - Aprofundando o conhecimento/calc.js
@@ -0,0 +1,79 @@
+function calc() {
+const operation = prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)\n');
+
+
+
+
+let n1 = prompt('Insira o primeiro valor:')
+let n2 = prompt('Insira o segundo valor:')
+let result;
+
+function soma() {
+result = Number(n1) + Number(n2);
+alert(`${n1} + ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+function subtr() {
+result = n1 - n2;
+alert(`${n1} - ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+function mult() {
+result = n1 * n2;
+alert(`${n1} * ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+function divReal() {
+result = n1 / n2;
+alert(`${n1} / ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+function divInt() {
+result = n1 % n2;
+alert(`${n1} % ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+function poten() {
+result = n1 ** n2;
+alert(`${n1} ** ${n2} = ${result}`)
+document.body.innerHTML = result
+
+}
+
+///
+
+if(operation == 1) {
+
+    soma()
+    
+    } else if(operation == 2) {
+    
+        subtr()
+    } else if(operation == 3) {
+    
+        mult()
+    
+    } else if( operation == 4) {
+    
+        divReal()
+    
+    
+    } else if(operation == 5) {
+    
+    
+        divInt()
+    } else if( operation == 6) {
+    
+        poten()
+    }
+    
+
+}
+
+
+calc()

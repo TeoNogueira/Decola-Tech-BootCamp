9e56d3c3f08ee54a9b68b2551a368512efbaac22
Teo Nogueira <teonogueira17@gmail.com>
2022-05-09 21:03:43 -0300

    ADD FUNCTIONS && PARAMS

---
+++ Sintaxe Básica Javascript/4 - Funções e suas particularidades/func_.js
@@ -0,0 +1,31 @@
+function test1() {
+
+console.log('Test 1')
+
+}
+
+// test1()
+
+
+function test2(food) {
+
+
+    return food
+
+}
+
+
+const text = 'Você se alimentou com:'
+
+
+const result = console.log(text, test2('Maçã!'));
+
+
+
+function msg(a, b) {
+
+    console.log(a, b)
+}
+
+
+msg('Decola Tech', '3a')

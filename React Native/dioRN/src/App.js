57acaa763fa99c2259a08ba1279adfedb93f2657
Teo Nogueira <teonogueira17@gmail.com>
2022-05-17 15:17:12 -0300

    finished_module_01_react_native

--- React Native/dioRN/src/App.js
+++ React Native/dioRN/src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet, SafeAreaView } from 'react-native'
+import {
+     View,
+      Image,
+       Text,
+        StyleSheet,
+         SafeAreaView,
+            Pressable,
+                Linking } from 'react-native';
 
 const colorGithub = '#010409'
 const imageGithub = 'https://avatars.githubusercontent.com/u/53917980?v=4'
 const colorFontGithub = '#c9d1d9';
+const colorDarkFontGithub = '#4F565E';
+const urlGithub = 'https://github.com/TeoNogueira'
+
 const App = () => {
+
+    const handlePressGoToGithub = async () => {
+
+     const res =  await  Linking.canOpenURL(urlGithub);
+
+     if(res) {
+
+        await Linking.openURL(urlGithub)
+     }
+    }
 
     return (
 
         <SafeAreaView style={style.container}>
-            <StatusBar accessibilityLabel='Teo Nogueira Develoepr' backgroundColor={colorGithub} barStyle='dark-content'/>
+            <StatusBar accessibilityLabel='Teo Nogueira Developer' backgroundColor={colorGithub} barStyle='dark-content'/>
         <View style={style.content}>
         {/* <Text style={style.text}>Hello World!</Text> */}
         <Image style={style.avatar} source={{uri:imageGithub}}/>
 
-        <Text style={[style.defaultText, style.name]}>Teo Nogueira</Text>
-        <Text style={[style.defaultText, style.nickName]}>teomuniz</Text>
-        <Text style={[style.defaultText, style.description]}>
+        <Text accessibilityLabel='Teo Nogueira | TEXT' style={[style.defaultText, style.name]}>Teo Nogueira</Text>
+
+        <Text accessibilityLabel='Teo Muniz | NICKNAME'  style={[style.defaultText, style.nickName]}>teomuniz</Text>
+
+        <Text accessibilityLabel='Teo Nogueira | DESCRIPTION'  style={[style.defaultText, style.description]}>
         Hello my name is Teófilo Nogueira currently studying at EBAC through the front-end engineering course, i develop front-end web applications, i love technology and I have been looking for daily knowledge, i am willing to face any challenges as a developer.
         </Text>
+        <Pressable onPress={handlePressGoToGithub}>
+
+        <View style={style.button}>
+
+            <Text style={[style.defaultText, style.textButton]}>Open in Github</Text>
+        </View>
+        </Pressable> 
+
      </View>
      </SafeAreaView>
 
@@ -57,8 +87,10 @@ const style = StyleSheet.create({
 
     },
     name: {
+        marginTop: 20,
         fontWeight: 'bold',
         fontSize: 24,
+        color: colorDarkFontGithub,
 
     },
 
@@ -67,9 +99,22 @@ const style = StyleSheet.create({
         fontSize: 18,
     },
     description: {
-
+        fontWeight: 'bold',
         fontSize: 14,
-    }
+    },
+
+    button: {
+        marginTop: 20,
+        backgroundColor: colorDarkFontGithub,
+        borderRadius: 10,
+        padding: 20,
+
+    },
+
+    textButton: {
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
 
     // text: {
 
